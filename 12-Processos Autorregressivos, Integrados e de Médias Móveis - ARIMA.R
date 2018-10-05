2f26ba858ff2340d7c9fc1f12cf7bbca3e2303ec
deisoka <42552313+deisoka@users.noreply.github.com>
2018-10-05 20:16:32 -0300

    Add files via upload

--- 12-Processos Autorregressivos, Integrados e de Médias Móveis - ARIMA.R
+++ 12-Processos Autorregressivos, Integrados e de Médias Móveis - ARIMA.R
@@ -83,7 +83,7 @@ arima021 <- arima(emprego, c(0,2,1))
 arima022 <- arima(emprego, c(0,2,2))
 arima023 <- arima(emprego, c(0,2,3))
 #AR
-arima0120 <- arima(emprego, c(1,2,0))
+arima120 <- arima(emprego, c(1,2,0))
 
 #Escolher o melhor modelo com base no menor AIC/BIC
 estimacoes <- list(arima123,arima120,arima121,
@@ -100,5 +100,126 @@ Resultados <- data.frame(Modelo,AIC,BIC)
 
 #Análise para o Câmbio
 
+plot(CAMBIO, type = "l")                            
+CAMBIO <- ts(CAMBIO, start = 1994, frequency = 1)  
+plot(CAMBIO, main="Cambio", 
+     ylab="Cambio", 
+     xlab="Ano") 
+
+acf(CAMBIO)                                          
+pacf(CAMBIO)                                         
+reglinEMP <- lm(CAMBIO ~ Anos)                      
+reglinEMP                                            
+plot(CAMBIO)                                       
+abline(reglinEMP, col="Blue")    
+
+residuosEMP <- reglinEMP$residuals                    
+reglinEMPres <- lm(residuosEMP ~ Anos)                
+plot(residuosEMP,type="l")                           
+abline(reglinEMPres, col="Blue")   
+
+pdcambio <- diff(CAMBIO)                                
+diferenca1 <- (data.frame(CAMBIO[2:18],pdcambio))       
+DIFERENCA <- ts(diferenca1, start = 1994, frequency = 1)  
+plot(DIFERENCA, plot.type="single", col=c("Black","Green")) 
+plot(pdcambio, type="l")                                   
+                 
+pdcambio1 <- diff(CAMBIO)                                           
+TesteDF_cambio1_trend <- ur.df(pdcambio1, "trend", lags = 1)         
+summary(TesteDF_cambio1_trend) 
+
+pdcambio2 <- diff(diff(CAMBIO))                                      
+TesteDF_cambio2_trend <- ur.df(pdcambio2, "trend", lags = 1)        
+summary(TesteDF_cambio2_trend) 
+
+arima123 <- arima(CAMBIO, c(1,2,3))
+
+arima120 <- arima(CAMBIO, c(1,2,0))
+arima121 <- arima(CAMBIO, c(1,2,1))
+arima122 <- arima(CAMBIO, c(1,2,2))
+
+arima220 <- arima(CAMBIO, c(2,2,0))
+arima221 <- arima(CAMBIO, c(2,2,1))
+arima222 <- arima(CAMBIO, c(2,2,2))
+arima223 <- arima(CAMBIO, c(2,2,3))
+
+arima021 <- arima(CAMBIO, c(0,2,1))
+arima022 <- arima(CAMBIO, c(0,2,2))
+arima023 <- arima(CAMBIO, c(0,2,3))
+
+arima120 <- arima(CAMBIO, c(1,2,0))
+
+estimacoes <- list(arima123,arima120,arima121,
+                   arima122,arima220,arima221,
+                   arima222,arima223,arima021,arima021, arima022,
+                   arima023,arima120)
+AIC <- sapply(estimacoes, AIC)
+BIC <- sapply(estimacoes, BIC)
+Modelo <-c(list("arima123","arima120","arima121",
+                "arima122","arima220","arima221",
+                "arima222","arima223","arima021","arima021", "arima022",
+                "arima023","arima120")) 
+Resultados <- data.frame(Modelo,AIC,BIC)
+
 #Análise para o PIB
 
+plot(PIB, type = "l")                            
+PIB <- ts(PIB, start = 1994, frequency = 1)  
+plot(PIB, main="PIB", 
+     ylab="PIB", 
+     xlab="Ano")
+
+acf(PIB)                                          
+pacf(PIB)                                         
+reglinEMP <- lm(PIB ~ Anos)                      
+reglinEMP                                            
+plot(PIB)                                       
+abline(reglinEMP, col="Blue")    
+
+residuosEMP <- reglinEMP$residuals                    
+reglinEMPres <- lm(residuosEMP ~ Anos)                
+plot(residuosEMP,type="l")                           
+abline(reglinEMPres, col="Blue")   
+
+pdpib <- diff(PIB)                                
+diferenca1 <- (data.frame(PIB[2:18],pdpib))       
+DIFERENCA <- ts(diferenca1, start = 1994, frequency = 1)  
+plot(DIFERENCA, plot.type="single", col=c("Black","Green")) 
+plot(pdpib, type="l")                                   
+
+pdpib1 <- diff(PIB)                                           
+TesteDF_pib1_trend <- ur.df(pdpib1, "trend", lags = 1)         
+summary(TesteDF_pib1_trend) 
+
+pdpib2 <- diff(diff(PIB))                                      
+TesteDF_pib2_trend <- ur.df(pdpib2, "trend", lags = 1)        
+summary(TesteDF_pib2_trend) 
+
+arima123 <- arima(PIB, c(1,2,3))
+
+arima120 <- arima(PIB, c(1,2,0))
+arima121 <- arima(PIB, c(1,2,1))
+arima122 <- arima(PIB, c(1,2,2))
+
+arima220 <- arima(PIB, c(2,2,0))
+arima221 <- arima(PIB, c(2,2,1))
+arima222 <- arima(PIB, c(2,2,2))
+arima223 <- arima(PIB, c(2,2,3))
+
+arima021 <- arima(PIB, c(0,2,1))
+arima022 <- arima(PIB, c(0,2,2))
+arima023 <- arima(PIB, c(0,2,3))
+
+arima120 <- arima(PIB, c(1,2,0))
+
+estimacoes <- list(arima123,arima120,arima121,
+                   arima122,arima220,arima221,
+                   arima222,arima223,arima021,arima021, arima022,
+                   arima023,arima120)
+AIC <- sapply(estimacoes, AIC)
+BIC <- sapply(estimacoes, BIC)
+Modelo <-c(list("arima123","arima120","arima121",
+                "arima122","arima220","arima221",
+                "arima222","arima223","arima021","arima021", "arima022",
+                "arima023","arima120")) 
+Resultados <- data.frame(Modelo,AIC,BIC)
